8260f47be0c54941bce20018a5c2622c411d5c29
saracart <57101507+saracart@users.noreply.github.com>
2019-12-02 15:51:45 +0100

    Add files via upload

---
+++ test/params.sh
@@ -0,0 +1,16 @@
+working_directory: /home/sarajorge/PIPECHIP/tarea1_FUL
+number_of_samples: 4
+genome: ftp://ftp.ensemblgenomes.org/pub/release-45/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz
+annotation: ftp://ftp.ensemblgenomes.org/pub/release-45/plants/gtf/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.45.gtf.gz
+chip_num: 2
+input_num: 2
+
+chip_1: SRR3288009
+chip_2: SRR3288010
+input_1: SRR3288011
+input_2: SRR3288012
+
+sample_dir: /home/sarajorge/PIPECHIP/test
+
+promoter: 1000
+output: peak_processing
---
+++ test/params_cp.sh
@@ -0,0 +1,15 @@
+working_directory: /home/sarajorge/PIPECHIP/tarea1_SHL
+number_of_samples: 4
+genome: ftp://ftp.ensemblgenomes.org/pub/release-45/plants/fasta/arabidopsis_thaliana/dna/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa.gz
+annotation: ftp://ftp.ensemblgenomes.org/pub/release-45/plants/gtf/arabidopsis_thaliana/Arabidopsis_thaliana.TAIR10.45.gtf.gz
+chip_num: 2
+input_num: 2
+
+chip_1: SRR6111265
+chip_2: SRR6111266
+input_1: SRR6111267
+input_2: SRR6111268
+
+
+sample_dir: /home/sarajorge/PIPECHIP/test
+promoter: 1000
